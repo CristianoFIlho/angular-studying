0c38ad4ebf75243ed6dcda26f0316b39576d732c
Cristiano Filho <54041918+CristianoFIlho@users.noreply.github.com>
2021-11-08 15:06:10 -0300

    fist git project

--- angular/aplication/aplication/karma.conf.js
+++ angular/aplication/aplication/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app-book'),
+      dir: require('path').join(__dirname, './coverage/aplication'),
       subdir: '.',
       reporters: [
         { type: 'html' },
