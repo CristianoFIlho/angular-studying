c5892259aab837f5533d769a9456d54cdc3d27bd
Cristiano Filho <54041918+CristianoFIlho@users.noreply.github.com>
2021-11-09 13:52:02 -0300

    table angular

--- angular/aplication/aplication/src/app/components/componentlist.ts
+++ angular/aplication/aplication/src/app/components/componentlist.ts
@@ -1,11 +1,49 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { Course } from './component';
 @Component({
   selector: 'app-componentlist',
   templateUrl: './componentlist.component.html'
   
 })
 
-export class ComponentList {
-   
+export class ComponentList implements OnInit {
+    components: Course[] = [];
+    
+    ngOnInit(): void {
+        this.components = [
+            {
+                id: 1,
+                name: 'Angular',
+                imageUrl: 'https://angular.io/assets/images/logos/angular/angular.svg',
+                
+                price: 100,
+                code: '123-weq',
+                duration: 2313,
+                rating: 10,
+                releaseDate: '12-12-12'
+            },
+            {
+                id: 2,
+                name: 'React',
+                imageUrl: 'https://angular.io/assets/images/logos/angular/angular.svg',
+
+                price: 1001,
+                code: '123-wet',
+                duration: 2300,
+                rating: 10,
+                releaseDate: '12-12-11'
+            },
+            {
+                id: 3,
+                name: 'Vue',
+                imageUrl: 'https://angular.io/assets/images/logos/angular/angular.svg',
+                price: 101,
+                code: '123-wea',
+                duration: 2200,
+                rating: 10,
+                releaseDate: '12-12-10'
+                
+            }
+        ];
+    }
 }
